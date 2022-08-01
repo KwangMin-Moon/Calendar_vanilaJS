d424f9c6ea381328aaa8538472f6c8e6e6458077
Jason <timeli@naver.com>
2022-08-02 00:27:43 +0900

    달력 타이틀 완성

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const $dayOfToday = document.querySelector('.day_of_today');
 const $dateOfToday = document.querySelector('.date_of_today');
+const $calenderTitle = document.querySelector('#calender_title');
 const days = ['SUN', 'MON', 'THE', 'WED', 'THU', 'FRI', '토'];
+const months = [
+  'JAN',
+  'FEB',
+  'MAR',
+  'APR',
+  'MAY',
+  'JUN',
+  'JUL',
+  'AUG',
+  'SEP',
+  'OCT',
+  'NOV',
+  'DEC',
+];
 const now = new Date();
 const day = now.getDay();
 const date = now.getDate();
+const year = now.getFullYear();
+const month = now.getMonth();
 
 $dayOfToday.textContent = days[day];
 $dateOfToday.textContent = date;
+$calenderTitle.textContent = `${months[month]} ${year}`;
