c96d6f5a84bb8c1dd6e5e15d47e06836aedf9234
Jason <timeli@naver.com>
2022-08-02 17:42:26 +0900

    rightBtn click event 완료

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const $dayOfToday = document.querySelector('.day_of_today');
 const $dateOfToday = document.querySelector('.date_of_today');
 const $calenderTitle = document.querySelector('#calender_title');
+const $td = document.querySelectorAll('td');
+const $rightBtn = document.querySelector('#right_btn');
 const days = ['SUN', 'MON', 'THE', 'WED', 'THU', 'FRI', '토'];
 const months = [
   'JAN',
@@ -19,9 +21,41 @@ const months = [
 const now = new Date();
 const day = now.getDay();
 const date = now.getDate();
-const year = now.getFullYear();
-const month = now.getMonth();
+let year = now.getFullYear();
+let month = now.getMonth();
+let startOfMonth = new Date(year, month, 1);
+let lastOfMonth = new Date(year, month + 1, 0);
+let startDay = startOfMonth.getDay();
+let lastDate = lastOfMonth.getDate();
 
 $dayOfToday.textContent = days[day];
 $dateOfToday.textContent = date;
 $calenderTitle.textContent = `${months[month]} ${year}`;
+
+function makeCalendar(startDay, lastDate) {
+  for (let i = 0; i < $td.length; i++) {
+    $td[i].textContent = '';
+  }
+  for (let i = 1; i < lastDate + 1; i++) {
+    $td[startDay + i - 1].textContent = i;
+  }
+  $td[date].style.color = 'red';
+}
+makeCalendar(startDay, lastDate);
+
+$rightBtn.addEventListener('click', () => {
+  month++;
+  if (month > 11) {
+    month = 0;
+    year++;
+  }
+  startOfMonth = new Date(year, month, 1);
+  lastOfMonth = new Date(year, month + 1, 0);
+  startDay = startOfMonth.getDay();
+  console.log(startDay);
+  lastDate = lastOfMonth.getDate();
+  console.log(lastDate);
+  makeCalendar(startDay, lastDate);
+  $td[date].style.color = 'black';
+  $calenderTitle.textContent = `${months[month]} ${year}`;
+});
